e68a4e27eaf32e4c0d0a42809bad598ad774475e
meytal benchimol <meytal09@gmail.com>
2021-10-19 18:43:58 +0300

    add scale to rectangle

--- include/Rectangle.h
+++ include/Rectangle.h
@@ -7,9 +7,10 @@ class Rectangle
 public:
 	Rectangle(const Vertex& bottomLeft, const Vertex& topRight);
 	Rectangle(const Vertex vertices[2]);
-	//Rectangle(double x0, double y0, double x1, double y1);
-	//Rectangle(const Vertex& start, double width, double height);
-	
+	/*
+	Rectangle(double x0, double y0, double x1, double y1);
+	Rectangle(const Vertex& start, double width, double height);
+	*/
 	Vertex getBottomLeft() const;
 	Vertex getTopRight() const;
 	double getWidth() const;
--- src/Rectangle.cpp
+++ src/Rectangle.cpp
@@ -1,14 +1,34 @@
 #include "Rectangle.h"
 #include "Board.h"
 
+const Vertex DEFAULT_BOTTOM_LEFT = Vertex(20,10),
+			 DEFAULT_TOP_RIGHT = Vertex(20,30);
+
 Rectangle::Rectangle(const Vertex& bottomLeft, const Vertex& topRight)
-	: m_bottomLeft(bottomLeft), m_topRight(topRight)
-{}
+	: m_bottomLeft(DEFAULT_BOTTOM_LEFT), m_topRight(DEFAULT_TOP_RIGHT)
+{
+	m_bottomLeft = bottomLeft;
+	m_topRight = topRight;
+}
 
 Rectangle::Rectangle(const Vertex vertices[2])
-	: m_bottomLeft(vertices[0]), m_topRight(vertices[1])
-{}
+	: m_bottomLeft(DEFAULT_BOTTOM_LEFT), m_topRight(DEFAULT_TOP_RIGHT)
+{
+	m_bottomLeft = vertices[0];
+	m_topRight = vertices[1];
+}
 
+/*
+Rectangle(double x0, double y0, double x1, double y1)
+{
+
+}
+
+Rectangle(const Vertex& start, double width, double height)
+{
+
+}
+*/
 // this function returns the bottom-left vertex of rectangle
 Vertex Rectangle::getBottomLeft() const
 {
@@ -81,6 +101,20 @@ Vertex Rectangle::getCenter() const
 // Documentation
 bool Rectangle::scale(double factor)
 {
+	Vertex center = getCenter();
 
+	double length_x = (center.m_col - m_bottomLeft.m_col) * factor,
+	       length_y = (center.m_row - m_bottomLeft.m_row) * factor;
+
+	Vertex new_bottomLeft = Vertex(center.m_col - length_x, center.m_row + length_y);
+	Vertex new_topRight = Vertex(center.m_col - length_x, center.m_row - length_y);
+
+	if (factor < 0 || !new_bottomLeft.isValid() || !new_topRight.isValid())
+		return false;
+	
+	m_bottomLeft = new_bottomLeft;
+	m_topRight = new_topRight;
+
+	return true;
 }
 //-----------------------------------------------------------------------------
